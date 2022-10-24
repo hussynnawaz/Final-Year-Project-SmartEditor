ff4be56b0f9d8f93474d6358429d692666c4b83f
AhmedSatti0 <ahmedsatti319@gmail.com>
2022-10-23 17:38:31 -0700

    Added button

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+      >
+    
+      </Button>
     </View>
   );
 }
