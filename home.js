a85e4791e19adc9573f3a3735bee3912e1d6a560
Hussain Ahmed <hussyn.nawaz@gmail.com>
2022-12-03 16:04:49 +0500

    Home Screen Navigation

--- home.js
+++ home.js
@@ -1,7 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Button, Text, View } from 'react-native';
-
 export default function App() {
   return (
-    
-  )
+<View>
+<Button
+  title="GET STARTED"
+  color="#FF2D55"
+/>
+</View>
+
+  
+  }
