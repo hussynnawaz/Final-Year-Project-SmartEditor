7ee21f07b19e2ebb1b14bf9df90c4ff3c45e3db1
Hussain Ahmed <hussyn.nawaz@gmail.com>
2022-12-05 19:59:09 +0500

    Update buttons

--- fivepage.js
+++ fivepage.js
@@ -1,46 +1,36 @@
-import React from 'react';
-import { StyleSheet, Button, View, SafeAreaView } from 'react-native';
+import React from "react";
+import { StyleSheet, Button, View, SafeAreaView } from "react-native";
 
-const Separator = () => (
-  <View style={styles.separator} />
-);
+const Separator = () => <View style={styles.separator} />;
 
 const App = () => (
   <SafeAreaView style={styles.container}>
-    <View>
+    {/* <View>
       
       <Button
         title="OPEN PDF"
        color="#FF2D55"
       />
-    </View>
-    <Separator />
+    </View> */}
+    {/* <Separator /> */}
     <View>
-      
-      <Button
-        title="EDIT PDF"
-        color="#FF2D55"   
-      />
+      {/* This button should upload a file from user in PDF Format only, and then user will be given some templates, option to directly edit text on any section of document*/}
+      <Button title="EDIT PDF" color="#FF2D55" onPress={uploadfile} />
     </View>
-
-    
   </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     marginHorizontal: 110,
-    
-  
   },
-  
+
   separator: {
     marginVertical: 10,
-    borderBottomColor: '#F0F0F0',
+    borderBottomColor: "#F0F0F0",
     borderBottomWidth: StyleSheet.hairlineWidth,
-    
   },
 });
 
