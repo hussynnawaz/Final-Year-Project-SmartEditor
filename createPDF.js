0e3cdc09b9d2f2985cc50e9a912b8ef99cfa4282
Hussain Ahmed <hussyn.nawaz@gmail.com>
2022-12-19 20:03:27 +0500

    Create a PDF and Modify PDF

---
+++ createPDF.js
@@ -0,0 +1,10 @@
+<script>
+    createPdf();
+    async function createPdf() {
+      const pdfDoc = await PDFLib.PDFDocument.create();
+      const page = pdfDoc.addPage([350, 400]);
+      page.moveTo(110, 200);
+      page.drawText('Hello World!');
+      const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+      document.getElementById('pdf').src = pdfDataUri;
+</script>
---
+++ modifyDocument.js
@@ -0,0 +1,23 @@
+import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+
+async function modifyPdf() {
+  const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf'
+  const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+
+  const pdfDoc = await PDFDocument.load(existingPdfBytes)
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+
+  const pages = pdfDoc.getPages()
+  const firstPage = pages[0]
+  const { width, height } = firstPage.getSize()
+  firstPage.drawText('This text was added with JavaScript!', {
+    x: 5,
+    y: height / 2 + 300,
+    size: 50,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+    rotate: degrees(-45),
+  })
+
+  const pdfBytes = await pdfDoc.save()
+}
